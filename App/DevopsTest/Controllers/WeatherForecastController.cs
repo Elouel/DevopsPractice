d4ce2d8476d328e769a438737322d130806c54f5
Ivelin Evtimov <elouel123@gmail.com>
2022-01-09 19:23:28 +0200

    test app

--- App/DevopsTest/Controllers/WeatherForecastController.cs
+++ App/DevopsTest/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace DevopsTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 7).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
