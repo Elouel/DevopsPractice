c4d9fc09587f196a86a40aa41cf63850eafee96f
Ivelin Evtimov <elouel123@gmail.com>
2022-01-24 19:13:53 +0200

    Change range

--- App/DevopsTest/Controllers/WeatherForecastService.cs
+++ App/DevopsTest/Controllers/WeatherForecastService.cs
@@ -9,7 +9,7 @@
 
         public WeatherForecast[] GetWeatherForecast()
         {
-            return Enumerable.Range(1, 3).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 4).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
